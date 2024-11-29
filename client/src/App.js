22ec249c4f0fa93cc82a7a94e15a281b5f0fded4
Anna Grigorovich <grigorovichx@gmail.com>
2024-11-29 17:45:04 +0300

    Cleanup4

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import MonsterList from './components/MonsterList';
-import MonsterDetails from './components/MonsterDetails';
-import MonsterEdit from './components/MonsterEdit';
 import MonsterForm from './components/MonsterForm';
 import Navbar from './components/Navbar';
 import BattlePage from './components/BattlePage';
@@ -17,10 +15,7 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MonsterList />} />
-          
-          <Route path="/monster/:id" element={<MonsterDetails />} />
           <Route path="/create" element={<MonsterForm />} />
-          <Route path="/edit/:id" element={<MonsterEdit />} />
           <Route path="/battle/:id" element={<BattlePage />} />
 
         </Routes>
--- server/config/db.js
+++ server/config/db.js
@@ -11,7 +11,7 @@ const connectDB = async () => {
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
-    process.exit(1); // Остановить процесс при ошибке подключения
+    process.exit(1);
   }
 };
 
